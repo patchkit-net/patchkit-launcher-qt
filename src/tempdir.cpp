3b41e8e8e6ff01703cd9d16aa67ceb4c61570929
Tomasz Jaworski <wiedzminek112@gmail.com>
2016-09-17 20:13:47 +0200

    Added class for handling temporary directories

---
+++ src/tempdir.cpp
@@ -0,0 +1,44 @@
+/*
+* Copyright (C) Upsoft 2016
+* License: https://github.com/patchkit-net/patchkit-launcher-qt/blob/master/LICENSE
+*/
+
+
+#include "tempdir.h"
+
+#include <QDir>
+
+TempDir::TempDir(const QString& t_path) : m_path(t_path)
+{
+    create();
+}
+
+TempDir::~TempDir()
+{
+    remove();
+}
+
+void TempDir::create() const
+{
+    QDir dir(m_path);
+
+    QFileInfo info(m_path);
+
+    if (info.isDir())
+    {
+        dir.removeRecursively();
+    }
+    else if (info.isFile())
+    {
+        QFile::remove(m_path);
+    }
+
+    dir.mkpath(".");
+}
+
+void TempDir::remove() const
+{
+    QDir dir(m_path);
+
+    dir.removeRecursively();
+}
